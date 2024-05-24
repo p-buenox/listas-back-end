ee55311ba6f5e8675e93330c5fa54d20d4f19497
pierre <pierre.bueno@escola.pr.gov.br>
2024-05-24 09:51:49 -0300

    arquivos da lista 1

---
+++ lista1/lista1.js
@@ -0,0 +1,9 @@
+const string1 = "inicio do programa"
+const string2 = "fim"
+
+const inicio = string1;
+const fim = string2;
+
+console.log(inicio);
+console.log(fim);
+
---
+++ lista1/lista2.js
@@ -0,0 +1,8 @@
+const myNumber = 3;
+
+const primeiroNumero = 3;
+const segundoNumero = 2;
+
+const soma = primeiroNumero + segundoNumero;
+console.log(soma);
+
